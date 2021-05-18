46caca5fc18319aac2bbb1ae910d25f34e86d560
Matjaz <matjaz.vuherer@gmail.com>
2021-05-18 02:07:49 +0200

    Controller now schedules cronbar for the next day

--- controller.sh
+++ controller.sh
@@ -3,5 +3,6 @@ crontab -l > newcron
 sed "/test.sh/d" ./newcron
 NUMBER_HOUR=$(shuf -i9-20 -n1)
 NUMBER_MIN=$(shuf -i0-59 -n1)
-echo $NUMBER_MIN $NUMBER_HOUR "* * * /path/to/controller.sh" >> newcorn
+DOW=$((($(date +%u)+1)%7))
+echo $NUMBER_MIN $NUMBER_HOUR "* *" $DOW "/path/to/controller.sh" >> newcorn
 
